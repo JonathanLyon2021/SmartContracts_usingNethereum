c1ce13f7a39cf745f0dc8b99fd59f24df13e179b
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-20 23:55:23 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,3 +4,10 @@ using System;
   using Nethereum.Web3.Accounts;
   using Nethereum.Hex.HexTypes;
   
+class Program {
+  class ContractService{
+    private readonly Web3 web3;
+    private readonly Contract contract;
+    private readonly Account account;
+    
+
