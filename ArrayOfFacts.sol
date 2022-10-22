9a0bd2f3f493d1da32de692cea1431c932b00ece
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-21 23:17:54 -0400

    Added public method/function getFacts()
    This is a view only method that takes a parameter index(uint256) and returns an index of an array called facts(string).

--- ArrayOfFacts.sol
+++ ArrayOfFacts.sol
@@ -4,21 +4,90 @@ pragma solidity >= 0.7.0 < 0.8.14;
 contract ArrayOfFacts {
     string[] private facts;
     address private contractOwner;
-     
-     constructor() {
+
+    constructor() {
         contractOwner = msg.sender;
     }
 
-     modifier onlyOwner() {
+    modifier onlyOwner() {
         require(msg.sender == contractOwner, "Only the contract owner can do this!");
         _;
     }
-    
+
     function add(string memory newFact) public onlyOwner {
         facts.push(newFact);
     }
-    
-     function count() public view returns(uint256) {
+
+    function count() public view returns(uint256) {
         return facts.length;
     }
+
+    function getFact(uint256 index) view public returns(string memory) {
+           return facts[index];          
+    }                                                                                                                                                                                                      
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
