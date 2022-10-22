d87167969bcd519c84cfa27ebc63296513af8374
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-21 22:46:44 -0400

    Added constructor
    Which makes the current caller of the contract(msg.sender) the contract owner.

--- ArrayOfFacts.sol
+++ ArrayOfFacts.sol
@@ -4,5 +4,9 @@ pragma solidity >= 0.7.0 < 0.8.14;
 contract ArrayOfFacts {
     string[] private facts;
     address private contractOwner;
+     
+     constructor() {
+        contractOwner = msg.sender;
+    }
 
 }
